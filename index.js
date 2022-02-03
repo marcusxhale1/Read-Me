fd273c029dde7297cf5208012befb6e72d30ce48
Marcus Hale <marcusxhale1@gmail.com>
2022-02-02 22:33:00 -0800

    I can add questions and answer them through this now. Working on the homework now

--- index.js
+++ index.js
@@ -14,12 +14,27 @@ const inquirer = require('inquirer');
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 //   });
 
-console.log(inquirer);
 
-const promptProject = portfolioData => {
-  // If there's no 'projects' array property, create one
-
-  
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+const promptProject = () => {
   console.log(`
 =================
 Add a New Project
@@ -29,7 +44,7 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the title of your project?'
+      message: 'What is the name of your project?'
     },
     {
       type: 'input',
@@ -60,7 +75,9 @@ Add a New Project
       default: false
     }
   ]);
-   
 };
-promptProject()
+
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
   .then(projectAnswers => console.log(projectAnswers));
